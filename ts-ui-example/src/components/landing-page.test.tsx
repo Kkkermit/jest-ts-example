4b119f4f95bf931fcafb99fa4006ac878325b15e
Joshua Eaton <jeaton446@gmail.com>
2024-10-21 12:51:54 +0100

    fix: Fixed failing test

--- ts-ui-example/src/components/landing-page.test.tsx
+++ ts-ui-example/src/components/landing-page.test.tsx
@@ -4,6 +4,7 @@ import Landing from "./landing-page";
 describe("renders learn react link", () => {
 	it("renders learn react link", () => {
 		render(<Landing />);
-		expect(screen.getByText("Something...")).toBeInTheDocument();
+		expect(screen.getByText("This epic ass thing was made by Kkermit")).toBeInTheDocument();
+		expect(screen.queryByText("This epic ass thing was NOT created by Kkermit")).not.toBeInTheDocument();
 	});
 });
